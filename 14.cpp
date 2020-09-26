8ca0b5ba125cfa60a380db45589745b666551b8d
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2020-09-27 03:34:20 +0900

    Longest Common Prefix

---
+++ 14.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {  
+        // no word in strs
+        if (strs.size() == 0){return "";}
+        //only one word in strs
+        else if (strs.size() == 1)
+            return strs[0];
+        
+        //fine the shortest one in strs
+        string s = strs[0];      
+        for (int i = 1;i < strs.size();i++){
+            if (s > strs[i]){s = strs[i];}
+        }
+        
+        //interate all words
+        for(int j = 0;j < strs.size();j++){
+            int n = 0;
+            //interate all characters 
+            for (int k = 0;k < s.size();k++){
+                //compare the characters one by one 
+                if (s[k] == strs[j][k]){n++;}
+                //no common prefix 
+                if (n == 0){return "";}         
+            }
+            //output the prefix
+            s = s.substr(0,n);
+        }
+        if (s.size() >0){return s;}
+        else {return "";}
+    }
+};
